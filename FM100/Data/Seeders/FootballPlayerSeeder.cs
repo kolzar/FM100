fd0b5ff1c65c03f2ac2d93de69877633b8211d44
UgoMezzogori <ugo.mezzogori@gmail.com>
2026-05-18 09:10:39 +0200

    centralized package management system for .NET.

--- FM100/Data/Seeders/FootballPlayerSeeder.cs
+++ FM100/Data/Seeders/FootballPlayerSeeder.cs
@@ -11,12 +11,10 @@ namespace FM100.Data.Seeders;
 public class FootballPlayerSeeder
 {
     private readonly IFootballPlayerRepository _repository;
-    private readonly Randomizer _randomizer;
 
     public FootballPlayerSeeder(IFootballPlayerRepository repository)
     {
         _repository = repository;
-        _randomizer = new Randomizer();
     }
 
     /// <summary>
@@ -48,76 +46,36 @@ public class FootballPlayerSeeder
             .RuleFor(p => p.Description, f => f.Lorem.Sentence(5))
             .RuleFor(p => p.Height, f => f.Random.Int(168, 200))
             .RuleFor(p => p.Weight, f => f.Random.Int(65, 95))
-            .RuleFor(p => p.ShirtNumber, f => f.Random.Int(1, 99).OrNull(f, 0.3f))
+            .RuleFor(p => p.ShirtNumber, f => f.Random.Int(1, 99).OrNull(f, 0.3f) ?? 0)
             .RuleFor(p => p.Potential, f => f.Random.Int(60, 99))
             .RuleFor(p => p.Reputation, f => f.Random.Int(1, 20))
             .RuleFor(p => p.MarketValue, f => f.Random.Int(1, 100))
-            .RuleFor(p => p.CurrentState, f => GenerateDynamicState() ?? new DynamicState())
-            .RuleFor(p => p.MentalAttributes, f => GenerateMentalAttributes() ?? new MentalAttributes())
+            .RuleFor(p => p.CurrentState, f => new DynamicState
+            {
+                Happiness = f.Random.Int(1, 20),
+                Anger = f.Random.Int(1, 20),
+                Fear = f.Random.Int(1, 20),
+                Sadness = f.Random.Int(1, 20),
+                Anxiety = f.Random.Int(1, 20),
+                Morale = f.Random.Int(1, 20)
+            })
+            .RuleFor(p => p.MentalAttributes, f => new MentalAttributes
+            {
+                Composure = f.Random.Int(1, 20),
+                Concentration = f.Random.Int(1, 20),
+                Leadership = f.Random.Int(1, 20),
+                Courage = f.Random.Int(1, 20),
+                Aggression = f.Random.Int(1, 20),
+                TacticalIntelligence = f.Random.Int(1, 20),
+                Resilience = f.Random.Int(1, 20),
+                Ambition = f.Random.Int(1, 20),
+                Discipline = f.Random.Int(1, 20),
+                Loyalty = f.Random.Int(1, 20),
+                PressureHandling = f.Random.Int(1, 20)
+            })
             .RuleFor(p => p.CurrentMatchEmotionalState, f => null);
 
         return faker.Generate(count) ?? new List<FootballPlayer>();
-    }
-
-    /// <summary>
-    /// Generates a random DynamicState for a player.
-    /// </summary>
-    private DynamicState GenerateDynamicState()
-    {
-        try
-        {
-            if (_randomizer == null)
-            {
-                _randomizer = new Randomizer();
-            }
-
-            return new DynamicState
-            {
-                Happiness = _randomizer.Int(1, 20),
-                Anger = _randomizer.Int(1, 20),
-                Fear = _randomizer.Int(1, 20),
-                Sadness = _randomizer.Int(1, 20),
-                Anxiety = _randomizer.Int(1, 20),
-                Morale = _randomizer.Int(1, 20)
-            };
-        }
-        catch
-        {
-            return new DynamicState();
-        }
-    }
-
-    /// <summary>
-    /// Generates random MentalAttributes for a player.
-    /// </summary>
-    private MentalAttributes GenerateMentalAttributes()
-    {
-        try
-        {
-            if (_randomizer == null)
-            {
-                _randomizer = new Randomizer();
-            }
-
-            return new MentalAttributes
-            {
-                Composure = _randomizer.Int(1, 20),
-                Concentration = _randomizer.Int(1, 20),
-                Leadership = _randomizer.Int(1, 20),
-                Courage = _randomizer.Int(1, 20),
-                Aggression = _randomizer.Int(1, 20),
-                TacticalIntelligence = _randomizer.Int(1, 20),
-                Resilience = _randomizer.Int(1, 20),
-                Ambition = _randomizer.Int(1, 20),
-                Discipline = _randomizer.Int(1, 20),
-                Loyalty = _randomizer.Int(1, 20),
-                PressureHandling = _randomizer.Int(1, 20)
-            };
-        }
-        catch
-        {
-            return new MentalAttributes();
-        }
     }
 
     /// <summary>
